bc9d5ec17a3006e78870bd6b5cdb5b372cbc61aa
Reeve <dcunhareeve@gmail.com>
2021-04-03 16:26:30 +1300

    point to heroku backend

--- frontend/src/Socket.js
+++ frontend/src/Socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:6001');
+//const socket = io('http://localhost:6001');
+const socket = io('https://reeved-codenames-backend.herokuapp.com/');
 
 export default socket;

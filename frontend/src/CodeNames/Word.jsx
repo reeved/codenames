3644f792fd205b32cae2015da4c227e3b2735b81
Reeve <dcunhareeve@gmail.com>
2021-04-05 00:00:21 +1200

    minor codenames logic fix

--- frontend/src/CodeNames/Word.jsx
+++ frontend/src/CodeNames/Word.jsx
@@ -51,10 +51,10 @@ const Word = ({ item }) => {
 
   const handleWordClick = (item) => {
     socket.emit('update-selected', item.id);
-    if (item.status === 'unsafe' || item.status !== currentTeam) {
-      socket.emit('change-turn', currentTeam);
-    } else if (item.status === 'bomb') {
+    if (item.status === 'bomb') {
       socket.emit('game-over');
+    } else if (item.status === 'unsafe' || item.status !== currentTeam) {
+      socket.emit('change-turn', currentTeam);
     } else if (item.status === 'Red' || item.status === 'Blue') {
       if ((item.status === 'Red' && gameState.redScore === 1) || (item.status === 'Blue' && gameState.blueScore === 1)) {
         socket.emit('game-over', item.status);
